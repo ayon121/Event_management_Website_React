3c7618f97ea19e3ecf675953a8e7dc3cfee951ee
ayon121 <borshonsaha1234@gmail.com>
2023-10-07 12:25:58 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwN9FATm2Um9dL_ozVbJ6qAIV1Vht57L8",
+  authDomain: "social-event-manager.firebaseapp.com",
+  projectId: "social-event-manager",
+  storageBucket: "social-event-manager.appspot.com",
+  messagingSenderId: "693205668169",
+  appId: "1:693205668169:web:378225c3f6cb18c5a4ca98"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
