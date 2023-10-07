bb5b5e4698bac029b0e755eb6042e4fbecb15a9e
ayon121 <borshonsaha1234@gmail.com>
2023-10-07 16:43:46 +0600

    banner section update with aos function

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,17 +2,22 @@
 // import PropTypes from 'prop-types';
 
 import Navber from "../Shared_Components/Navber";
-
+import Aos from "aos";
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 const Banner = () => {
+    useEffect(()=> {
+        Aos.init({duration : 800})
+    },[])
     return (
-        <div className="min-h-screen bg-[#EDEDED] rounded-e-3xl  font-Bebas mb-10 pb-7">
+        <div className="min-h-screen bg-[#EDEDED] rounded-br-3xl border-b-4 border-[#DA0037] font-Bebas mb-10 pb-7 lg:pb-4">
             <Navber></Navber>
             <div className="hero pt-9 px-4 w-full">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src='https://i.ibb.co/WvMFmTd/jakob-dalbjorn-cu-KJre3ny-Yc-unsplash.jpg' className="w-4/5 lg:w-1/3  mx-auto rounded-xl shadow-2xl " />
-                    <div>
-                        <h1 className="text-4xl md:text-5xl font-bold pt-3">Your Events, Our Expertise</h1>
-                        <p className="py-6 w-4/5">Turning Your Moments into Memories. We are the Architects of Joy, Crafting Every Celebration with Perfection. Because We made Events Extraordinary, Every Time</p>
+                    <img data-aos="fade-left" src='https://i.ibb.co/WvMFmTd/jakob-dalbjorn-cu-KJre3ny-Yc-unsplash.jpg' className="w-4/5 lg:w-1/3  mx-auto rounded-xl shadow-2xl " />
+                    <div data-aos="fade-up-right">
+                        <h1 className="text-4xl md:text-5xl font-bold pt-3 lg:pt-1">Your Events, Our Expertise</h1>
+                        <p className="py-6 w-4/5">Turning Your Moments into Memories. We are the Architects of Joy, Crafting Every Celebration with Perfection. Because We made Events Extraordinary, Every Time.</p>
                         <a className="px-3 py-2 md:px-5 md:py-3 rounded-lg bg-[#DA0037] text-white">B o o k  N o w</a>
                     </div>
                 </div>
---
+++ src/Component/Services/Services.jsx
@@ -0,0 +1,17 @@
+
+// import PropTypes from 'prop-types';
+
+const Services = () => {
+    return (
+        <div className='mt-7 font-Bebas text-center'>
+            <h1 className="text-3xl md:text-5xl mb-4">Our <span className="text-[#DA0037]">Services</span></h1>
+            
+        </div>
+    );
+};
+
+Services.propTypes = {
+    
+};
+
+export default Services;
